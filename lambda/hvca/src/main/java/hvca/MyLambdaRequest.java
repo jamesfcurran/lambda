bfc03c33696470c325d019037293093aedd28dd5
Jim Curran <jfcmail@embarqmail.com>
2018-08-16 19:02:02 -0400

    These are separate files to hold the Requst and Respnse classes used by the Amazon lambda stuff

---
+++ lambda/hvca/src/main/java/hvca/MyLambdaRequest.java
@@ -0,0 +1,19 @@
+package HVCA; 
+public class MyLambdaRequest
+{
+    String name;
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public void setName(String name) {
+        this.name = name;
+    }
+ 
+    @Override
+    public String toString() {
+        return "MyLambdaRequest [name=" + name + "]";
+    }
+}
+
